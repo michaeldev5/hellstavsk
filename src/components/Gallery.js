540857b9166cbe7b4eba985ff4e3b2b315a4aa74
Michael29 <absolon.mm@gmail.com>
2021-08-04 22:19:32 +0200

    Add gallery content #HS-14 State Done

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,10 @@
 import React from "react"
-import house1 from "../images/house1.png"
-import house2 from "../images/house2.png"
-import house3 from "../images/house3.png"
+import zlate from "../images/gallery/zlate.jpg"
+import richvald from "../images/gallery/richvald.jpg"
+import heppenheim from "../images/gallery/heppenheim.jpg"
+import hazlin from "../images/gallery/hazlin.jpg"
+import fribourg from "../images/gallery/fribourg.jpg"
+import GalleryItem from "./GalleryItem"
 import SectionHeader from "./SectionHeader"
 
 const Gallery = () => (
@@ -12,132 +15,36 @@ const Gallery = () => (
         description="Proin risus erat, volutpat hendrerit nibh vel, finibus venenatis urna. Integer vulputate turpis sed mauris euismod vestibulum. Etiam lacinia eleifend ante nec eleifend. Sed id arcu ac eros consectetur maximus. Nam hendrerit turpis nec convallis scelerisque."
       />
       <div className="flex flex-wrap -m-4">
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
-          <div className="flex relative">
-            <img
-              alt="gallery"
-              className="absolute inset-0 w-full h-full object-cover object-center"
-              src={house1}
-            />
-            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 className="tracking-widest text-sm title-font font-medium text-primary1 mb-1">
-                DOMOVÉ STAVBY
-              </h2>
-              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                Presklenný byt
-              </h1>
-              <p className="leading-relaxed">
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                diam nonumy.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
-          <div className="flex relative">
-            <img
-              alt="gallery"
-              className="absolute inset-0 w-full h-full object-cover object-center"
-              src={house2}
-            />
-            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 className="tracking-widest text-sm title-font font-medium text-primary1 mb-1">
-                THE SUBTITLE
-              </h2>
-              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                The Catalyzer
-              </h1>
-              <p className="leading-relaxed">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
-          <div className="flex relative">
-            <img
-              alt="gallery"
-              className="absolute inset-0 w-full h-full object-cover object-center"
-              src={house3}
-            />
-            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 className="tracking-widest text-sm title-font font-medium text-primary1 mb-1">
-                THE SUBTITLE
-              </h2>
-              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                The 400 Blows
-              </h1>
-              <p className="leading-relaxed">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
-          <div className="flex relative">
-            <img
-              alt="gallery"
-              className="absolute inset-0 w-full h-full object-cover object-center"
-              src={house1}
-            />
-            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 className="tracking-widest text-sm title-font font-medium text-primary1 mb-1">
-                THE SUBTITLE
-              </h2>
-              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                Neptune
-              </h1>
-              <p className="leading-relaxed">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
-          <div className="flex relative">
-            <img
-              alt="gallery"
-              className="absolute inset-0 w-full h-full object-cover object-center"
-              src={house1}
-            />
-            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 className="tracking-widest text-sm title-font font-medium text-primary1 mb-1">
-                THE SUBTITLE
-              </h2>
-              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                Holden Caulfield
-              </h1>
-              <p className="leading-relaxed">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="lg:w-1/3 sm:w-1/2 p-4">
-          <div className="flex relative">
-            <img
-              alt="gallery"
-              className="absolute inset-0 w-full h-full object-cover object-center"
-              src={house1}
-            />
-            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-              <h2 className="tracking-widest text-sm title-font font-medium text-primary1 mb-1">
-                THE SUBTITLE
-              </h2>
-              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                Alper Kamu
-              </h1>
-              <p className="leading-relaxed">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-            </div>
-          </div>
-        </div>
+        <GalleryItem
+          imageSrc={zlate}
+          title="Zlaté"
+          subtitle="Rodinný dom"
+          desc="Popis"
+        />
+        <GalleryItem
+          imageSrc={richvald}
+          title="Richvald"
+          subtitle="Rodinný dom s podkrovím"
+          desc="Popis"
+        />
+        <GalleryItem
+          imageSrc={heppenheim}
+          title="Heppenheim"
+          subtitle="Omietnutie garáže"
+          desc="Popis"
+        />
+        <GalleryItem
+          imageSrc={hazlin}
+          title="Hažlín"
+          subtitle="Rodinný dvojpodlažný dom"
+          desc="Popis"
+        />
+        <GalleryItem
+          imageSrc={fribourg}
+          title="Fribourg"
+          subtitle="Historické podkrovné izby"
+          desc="Popis"
+        />
       </div>
     </div>
   </section>
