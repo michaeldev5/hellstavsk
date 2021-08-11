987bd127d636d4e5b03d0e9f43ba57cf7fcd4fd8
Michael Absolon <absolon@onesimplicity.com>
2021-08-11 20:03:19 +0200

    Add Image carousel component

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,9 +1,8 @@
 import React from "react"
 import Nav from "../components/Nav"
 import navigation from "../navigation"
-import Gallery from "../components/Gallery/Gallery"
-import GalleryItem from "../components/Gallery/GalleryItem"
 import Footer from "../components/Footer"
+import Carousel from "../components/Carousel"
 
 const ProjectPage = ({
   pageContext: {
@@ -14,16 +13,12 @@ const ProjectPage = ({
   return (
     <>
       <Nav navigation={navigation} />
-      <div className="container mx-auto px-2">
+      <div className="container mx-auto px-4">
         <h1>{name}</h1>
         <h3>{subtitle}</h3>
         <p>{desc}</p>
       </div>
-      <Gallery>
-        {images.map(image => (
-          <GalleryItem imageSrc={image} />
-        ))}
-      </Gallery>
+      <Carousel images={images} />
       <Footer navigation={navigation} />
     </>
   )
