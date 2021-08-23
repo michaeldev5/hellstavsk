bec45080d1245d745a999ebbccba3cc8156f24a5
Michael29 <absolon.mm@gmail.com>
2021-08-23 15:12:38 +0200

    Make feature item smaller #HS-30 State Done

--- src/components/Features/FeatureItem.tsx
+++ src/components/Features/FeatureItem.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react"
+import React, { FC, ReactNode } from "react"
 
 type Props = {
   title: string
@@ -6,16 +6,16 @@ type Props = {
   icon: ReactNode
 }
 
-const FeatureItem = ({ title, content, icon }) => (
-  <div className="xl:w-1/3 md:w-1/2 p-4">
-    <div className="border border-gray-200 p-6 rounded-lg">
-      <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-primary1light text-primary1 mb-4">
+const FeatureItem: FC<Props> = ({ title, content, icon }) => (
+  <div className="xl:w-1/3 md:w-1/2 p-3">
+    <div className="border border-gray-200 p-5 rounded-lg">
+      <div className="w-9 h-9 inline-flex items-center justify-center rounded-full bg-primary1light text-primary1 mb-4">
         {icon}
       </div>
-      <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
+      <h2 className="text-base text-gray-900 font-medium title-font mb-2">
         {title}
       </h2>
-      <p className="leading-relaxed text-base">{content}</p>
+      <p className="leading-relaxed text-sm">{content}</p>
     </div>
   </div>
 )
