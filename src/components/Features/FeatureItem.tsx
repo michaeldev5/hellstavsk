83313e3d6a0b0eea0b9fe0e0113dcba10004cfa8
Michael29 <absolon.mm@gmail.com>
2021-08-24 18:24:40 +0200

    Add features section item shadow on hover #HS-31 State Done

--- src/components/Features/FeatureItem.tsx
+++ src/components/Features/FeatureItem.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const FeatureItem: FC<Props> = ({ title, content, icon }) => (
   <div className="xl:w-1/3 md:w-1/2 p-3">
-    <div className="border border-gray-200 p-5 rounded-lg">
+    <div className="border border-gray-200 p-5 rounded-lg hover:shadow-lg transition-all">
       <div className="w-9 h-9 inline-flex items-center justify-center rounded-full bg-primary1light text-primary1 mb-4">
         {icon}
       </div>
