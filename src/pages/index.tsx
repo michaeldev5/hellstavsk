a8229a89999bcc60ac5aeb70fc1c51d7e1359a4d
Michael29 <absolon.mm@gmail.com>
2021-08-24 11:04:32 +0200

    Hide Team section till its done #HS-28 State Done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Footer from "../components/Footer"
 import Projects from "../components/Projects"
 import MapWithContact from "../components/MapWithContact"
 import Nav from "../components/Nav"
-import Team from "../components/Team"
 import Testimonials from "../components/Testimonials"
 import navigation from "../navigation"
 import Features from "../components/Features/Features"
@@ -16,7 +15,6 @@ const IndexPage = () => (
     <Nav navigation={navigation} showHero />
     <Features />
     <Projects />
-    <Team />
     <Testimonials />
     <Download />
     <MapWithContact />
