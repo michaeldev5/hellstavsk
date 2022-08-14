4f252f7ff82f240e965e86bda20db4106f1c2bec
Michael Absolon <absolon.mm@gmail.com>
2022-08-14 20:20:41 +0200

    Update team

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -8,24 +8,30 @@ const Team = () => (
     title="Náš tím"
     description="Hellstav s.r.o. pozostáva z troch bratov, ktorí sa stavebnej činnosti venujú už viac ako 20 rokov. Môžete sa spoľahnúť, že vám okrém dobrých rád splnia vaše vízie."
   >
-    <div className="grid gap-10 mx-auto lg:grid-cols-3">
+    <div className="grid gap-10 mx-auto lg:grid-cols-2">
       <TeamMember
         name="Stanislav Helmanovský"
         desc="Spolumajiteľ"
         phone="0907278857"
-        imageSrc="https://www.pinpng.com/pngs/m/341-3415688_no-avatar-png-transparent-png.png"
+        imageSrc="team/stano.jpg"
       />
       <TeamMember
         name="Vladimír Helmanovský"
         desc="Spolumajiteľ"
         phone="0910144856"
-        imageSrc="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+        imageSrc="team/vlado.jpg"
       />
       <TeamMember
         name="Ján Helmanovský"
         desc="Spolumajiteľ"
         phone="0911846157"
-        imageSrc="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+        imageSrc="team/jan.jpg"
+      />
+      <TeamMember
+        name="Jakub Helmanovský"
+        desc="Spolumajiteľ"
+        phone="0911846157"
+        enableAvatar
       />
     </div>
   </Section>
--- src/components/TeamMember.tsx
+++ src/components/TeamMember.tsx
@@ -5,20 +5,24 @@ type Props = {
   name: string
   desc: string
   phone: string
-  imageSrc: string
+  imageSrc?: string
+  enableAvatar?: boolean
 }
 
-const TeamMember: FC<Props> = ({ name, desc, phone, imageSrc }) => (
+const TeamMember: FC<Props> = ({ name, desc, phone, imageSrc, enableAvatar = false }) => (
   <div className="grid grid-cols-3">
     <div className="relative aspect-[2/3] shadow">
-      {/* <img
-        className="absolute object-cover w-full h-full rounded-md"
-        src={imageSrc}
-        alt={name}
-      /> */}
-      <svg xmlns="http://www.w3.org/2000/svg" className="absolute object-cover w-full h-full rounded-md" viewBox="0 0 20 20" fill="currentColor">
-        <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
-      </svg>
+      {enableAvatar ? (
+        <svg xmlns="http://www.w3.org/2000/svg" className="absolute object-cover w-full h-full rounded-md" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+        </svg>
+      ) : (
+        <img
+          className="absolute object-cover w-full h-full rounded-md"
+          src={imageSrc}
+          alt={name}
+        />
+      )}
     </div>
     <div className="flex flex-col mt-3 pl-5 col-span-2">
       <p className="text-lg font-bold m-0 lg:w-2">{name}</p>
